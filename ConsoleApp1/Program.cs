5e4ba54e92891da7241b038ff14daf391e1e7a4a
Ivan Kyryliuk <s32785@pjwstk.edu.pl>
2026-03-11 13:13:03 +0200

    Added helper class, which processes input and gets output as a number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,20 @@ namespace Task1
 { 
     class InputHandler
     {
+        class StringToNumber
+        {
+            public StringToNumber(String input)
+            {
+                int parsed = int.Parse(input);
+                Console.WriteLine(parsed +100);
+            }
+        }
         static void Main(String [] args)
         {
             Console.Write("Please, enter a number: ");
             string input = Console.ReadLine();
             Console.WriteLine(input);
+            StringToNumber processer = new StringToNumber(input);
         }
     }
 }
